4f63207de5fdeb0d6328855abf6d0aa0408a3e86
matvey_makaro <matvey.makaro@gmail.com>
2022-10-26 13:59:31 +0300

    Added category selection.

--- app/src/main/java/com/example/converter/MainActivity.kt
+++ app/src/main/java/com/example/converter/MainActivity.kt
@@ -1,12 +1,36 @@
 package com.example.converter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val key = "unitGroupName"
+        val intentToConverterActivity = Intent(this, ConverterActivity::class.java)
+
+        val distanceButton = findViewById<Button>(R.id.button_distance)
+        distanceButton.setOnClickListener{
+            intentToConverterActivity.putExtra(key, R.array.distanceUnits)
+            startActivity(intentToConverterActivity)
+        }
+
+        val weightButton = findViewById<Button>(R.id.button_weight)
+        weightButton.setOnClickListener{
+            intentToConverterActivity.putExtra(key, R.array.weightUnits)
+            startActivity(intentToConverterActivity)
+        }
+
+        val speedButton = findViewById<Button>(R.id.button_speed)
+        speedButton.setOnClickListener{
+            intentToConverterActivity.putExtra(key, R.array.speedUnits)
+            startActivity(intentToConverterActivity)
+        }
+
     }
 }
